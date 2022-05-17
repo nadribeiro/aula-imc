d1b88aadcc5530549113c59cf05396e8bf9e9fcd
NADIR RIBEIRO <nadir-ribeiro@outlook.com>
2022-05-16 23:11:39 -0300

    Inicialização

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,9 @@
+import PageTitle from "../components/PageTitle";
+
+export default function AboutPage() {
+    return (
+        <div className="AboutPage">
+            <PageTitle title="Sobre" />
+        </div>
+    )
+}
